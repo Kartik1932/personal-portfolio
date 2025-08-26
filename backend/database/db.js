8aa740c0ac5058241e2b04355b2a2362936624b0
kartik <kartiknangla882@gmail.com>
2025-08-26 21:46:32 +0530

    Constructing DATABASE_URL in db.js for now

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,13 +1,20 @@
 const { Pool } = require('pg')
-require('dotenv').config({ path: '../.env'});
+require('dotenv').config();
 
+console.log('Environment variables:');
+
+console.log('POSTGRES_USER:', process.env.POSTGRES_USER);
+console.log('POSTGRES_PASSWORD:', process.env.POSTGRES_PASSWORD);
+console.log('POSTGRES_DB:', process.env.POSTGRES_DB);
+
+const DATABASE_URL = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost:5432/${process.env.POSTGRES_DB}`;
+console.log('DATABASE_URL:', DATABASE_URL);
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
-
+    connectionString: DATABASE_URL,
+});
 pool.connect((err, client, release) => {
     if(err){
-        console.error('Error connecting to db');
+        console.error('Error connecting to db', err.message);
     }else{
         console.log('Connected')
         release();
